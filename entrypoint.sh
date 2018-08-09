22cefcb442c6d896ce27d8a340774c8882f839b5
cristina-grosu <cristina.grosu@bigstep.com>
2018-08-09 15:02:51 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$CONTAINER_DIR" != "" ]; then
-   mkdir -p $CONTAINER_DIR
-   chmod 700 "$CONTAINER_DIR"
-   chown -R postgres "$CONTAINER_DIR"
-   mkdir -p /run/postgresql
-   chmod g+s /run/postgresql
-   chown -R postgres /run/postgresql
-   su-exec postgres initdb -D $CONTAINER_DIR
-   
-   mkdir /var/lib/postgresql
-   chmod 777 /var/lib/postgresql
-   chown -R postgres /var/lib/postgresql
-    
-   export PGDATA=$CONTAINER_DIR
-   export authMethod=md5 
-   
-   { echo; echo "host all all all $authMethod"; } | su-exec postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
-    
-   su-exec postgres pg_ctl -D "$PGDATA" \
-			-o "-c listen_addresses='*'" \
-			-w start
-         
-   psql=( psql -v ON_ERROR_STOP=1 )
+if [ ! -f /var/lib/pgadmin/pgadmin4.db ]; then
+    if [ -z "${PGADMIN_DEFAULT_EMAIL}" -o -z "${PGADMIN_DEFAULT_PASSWORD}" ]; then
+        echo 'You need to specify PGADMIN_DEFAULT_EMAIL and PGADMIN_DEFAULT_PASSWORD environment variables'
+        exit 1
+    fi
 
-	"${psql[@]}" --username postgres <<-EOSQL
-		ALTER USER postgres PASSWORD '$POSTGRES_PASSWORD' ;
-	EOSQL
-	echo
-		
-	psql+=( --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" )
+    # Set the default username and password in a
+    # backwards compatible way
+    export PGADMIN_SETUP_EMAIL=${PGADMIN_DEFAULT_EMAIL}
+    export PGADMIN_SETUP_PASSWORD=${PGADMIN_DEFAULT_PASSWORD}
 
-	echo
-      
-         su-exec postgres pg_ctl -D "$PGDATA" -m fast -w stop
-
-	 echo
-	 echo 'PostgreSQL init process complete; ready for start up.'
-	 echo
+    # Initialize DB before starting Gunicorn
+    # Importing pgadmin4 (from this script) is enough
+    python run_pgadmin.py
 fi
 
-# Start PostgreSQL as long-running process
-su-exec postgres postgres -D $PGDATA
+# NOTE: currently pgadmin can run only with 1 worker due to sessions implementation
+# Using --threads to have multi-threaded single-process worker
+
+if [ ! -z ${PGADMIN_ENABLE_TLS} ]; then
+    exec gunicorn --bind [::]:${PGADMIN_LISTEN_PORT:-443} -w 1 --threads ${GUNICORN_THREADS:-25} --access-logfile - --keyfile /certs/server.key --certfile /certs/server.cert run_pgadmin:app
+else
+    exec gunicorn --bind [::]:${PGADMIN_LISTEN_PORT:-80} -w 1 --threads ${GUNICORN_THREADS:-25} --access-logfile - run_pgadmin:app
+fi
